5d0bf6d8d352066b8ad090f8822e2749119b5f23
Abraão Vila Nova <abraaovilanova@gmail.com>
2021-11-28 16:21:47 -0300

    correção url prod

--- src/api/api.js
+++ src/api/api.js
@@ -2,4 +2,4 @@
 // export const url = 'http://localhost:3001'
 
 //Production
-export const url = 'https://essential-english-api.herokuapp.com/'
+export const url = 'https://essential-english-api.herokuapp.com'

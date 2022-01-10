e1a1d478859fc8655cc47416a1fff4783576fdf2
abraaovilanova <abraaovilanova@gmail.com>
2022-01-09 22:31:56 -0300

    barra de progresso

--- src/components/TagCard/TagCard.jsx
+++ src/components/TagCard/TagCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './TagCard.css'
 
 const TagCard = ({
@@ -10,6 +10,9 @@ const TagCard = ({
     setShowTags
 }) => {
 
+    const [rand, setRand] = useState(Math.floor(Math.random() * 100))
+
+
     const handleOnClick = () => {
         setActive(index)
         getSentenceByTag(tagName)
@@ -18,9 +21,14 @@ const TagCard = ({
     }
 
     return (
-        <div className="tag-btn" onClick={handleOnClick}>
-            <h3 className="btn-txt">{tagName}</h3>
-        </div>
+            <div className="tag-btn" onClick={handleOnClick}>
+                <h3 className="btn-txt">{tagName}</h3>
+                <div className="bar" style={{backgroundColor: "gray", borderRadius: "10px"}}>
+                    <div className="progressbar" style={{height:"24px", width:`${rand}%`, backgroundColor:"#d175b7", borderRadius: "10px"}}>
+                        {rand > 5? <div className="shaddow-progress"></div>: ''}
+                    </div>
+                </div>
+            </div>
     )
 }
 
